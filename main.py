0a11c020423b65a4be9743fa75b93b1548790d3c
Thomas Liu <liuxk99@gmail.com>
2026-05-13 10:05:04 +0800

    <Update>: data and figure by 05/13.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from jifen_plot import plot_scores
 
 plot_scores(score_data_csv=DEFAULT_SCORE_DATA_CSV, figure_file=DEFAULT_FIGURE_FILE,
             last_year_baseline=DEFAULT_LAST_YEAR_BASELINE, predicted_baseline=DEFAULT_PREDICTED_BASELINE)
-plot_scores(score_data_csv="jifen_data_2025.csv", figure_file=DEFAULT_FIGURE_FILE,
-            last_year_baseline=114.46, predicted_baseline=117.33)
-plot_scores(score_data_csv="jifen_data_2024.csv", figure_file=DEFAULT_FIGURE_FILE,
-            last_year_baseline=109.92, predicted_baseline=114.46)
-plot_scores(score_data_csv="jifen_data_2023.csv", figure_file=DEFAULT_FIGURE_FILE,
-            last_year_baseline=105.38, predicted_baseline=109.92)
+# plot_scores(score_data_csv="jifen_data_2025.csv", figure_file=DEFAULT_FIGURE_FILE,
+#             last_year_baseline=114.46, predicted_baseline=117.33)
+# plot_scores(score_data_csv="jifen_data_2024.csv", figure_file=DEFAULT_FIGURE_FILE,
+#             last_year_baseline=109.92, predicted_baseline=114.46)
+# plot_scores(score_data_csv="jifen_data_2023.csv", figure_file=DEFAULT_FIGURE_FILE,
+#             last_year_baseline=105.38, predicted_baseline=109.92)
